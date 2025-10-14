685fe456f06a6a911c4ac6a7133ceb4e687b7f6b
RohanAlief <rafarafa92653@gmail.com>
2025-10-14 20:20:30 +0700

    learn py early

---
+++ tugasEZ.py
@@ -0,0 +1,37 @@
+#soal 1 usia dewasa
+umur_andi = 16
+if umur_andi < 18 :
+    print("Andi belum boleh membuat KTP")
+else :
+    print("Andi sudah boleh membuat KTP")
+
+#soal 2 nilai lulus
+nilai_dina = 85
+if nilai_dina >= 70 :
+    print("Dina lulus ujian")
+else :
+    print("Dina tidak lulus ujian")
+
+#soal 3 uang cukup
+uang_ani = 45000
+harga_buku = 50000
+kurang_uang = harga_buku - uang_ani
+
+if uang_ani >= harga_buku :
+    print(f"Ani bisa membeli buku {harga_buku}")
+else :
+    print(f"Ani tidak bisa membeli buku, uangnya kurang {kurang_uang}")
+
+#soal 4 juara kelas
+avg_fajar = 92
+if avg_fajar >= 90 :
+    print("Fajar juara kelas")
+else :
+    print("Fajar bukan juara kelas")
+
+#soal 5 kecepatan aman
+vcar = 85
+if vcar > 80 :
+    print("Kecepatan mobil melebihi batas aman")
+else :
+    print("Kecepatan mobil aman")
